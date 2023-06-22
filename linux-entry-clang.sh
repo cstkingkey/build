41ed1052a7b02cdf87353bf09c571cabc3a7635f
root <root@localhost.localdomain>
2023-06-22 10:37:55 +0800

    abi

--- linux-entry-clang.sh
+++ linux-entry-clang.sh
@@ -11,8 +11,8 @@ export PATH=${LOCAL_TOOLCHAIN_ROOT}/bin:${PATH}:${CROSS_TOOLCHAIN_ROOT}/bin
 ## openssl-sys need it.
 export PKG_CONFIG_ALLOW_CROSS=1
 
-declare -x CROSS_TARGET=${ARCH}-unknown-linux-gnu
-declare -x CROSS_TARGET2=${ARCH}-linux-gnu
+declare -x CROSS_TARGET=${ARCH}-unknown-linux-gnu${ABI}
+declare -x CROSS_TARGET2=${ARCH}-linux-gnu${ABI}
 
 declare -x CFLTK_TOOLCHAIN="${root}/cross.cmake"
 
@@ -33,6 +33,6 @@ declare -x CARGO_TARGET_${upper_suffix}_LINKER=linux-clang
 
 # use env to avoid hacking pc files
 declare -x PKG_CONFIG_SYSROOT_DIR=${CROSS_TARGET_ROOT}
-declare -x PKG_CONFIG_PATH="${CROSS_TARGET_ROOT}/usr/lib/${ARCH}-linux-gnu/pkgconfig/:${CROSS_TARGET_ROOT}/usr/share/pkgconfig/:${PKG_CONFIG_PATH}"
+declare -x PKG_CONFIG_PATH="${CROSS_TARGET_ROOT}/usr/lib/${CROSS_TARGET2}/pkgconfig/:${CROSS_TARGET_ROOT}/usr/share/pkgconfig/:${PKG_CONFIG_PATH}"
 
 exec "$@"
