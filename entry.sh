3068e1dd9cf5b01a84d3b1c68b7d3b0a8c2dbab3
root <root@localhost.localdomain>
2023-06-23 14:01:31 +0800

    fix

--- entry.sh
+++ entry.sh
@@ -8,17 +8,18 @@ root="`readlink -f $root`"
 case ${OS} in
   linux)
       export REAL_ENTRY=$root/linux-entry-clang.sh
-      export RUST_TARGET=$arch-unknown-linux-gnu${abi}
-      export LINUX_TARGET=$arch-linux-gnu${abi}
+      export RUST_TARGET=$ARCH-unknown-linux-gnu${abi}
+      export LINUX_TARGET=$ARCH-linux-gnu${abi}
       ;;
   win)
       export REAL_ENTRY=$root/xwin-entry.sh
       export RUST_TARGET=${ARCH}-pc-windows-msvc
       if [[ ${ARCH} == "i"*"86" ]]
       then
-         declare -x WIN_ARCH="x86"
+        #delare -x is not working
+         export WIN_ARCH="x86"
       else
-         declare -x WIN_ARCH=${ARCH}
+         export WIN_ARCH=${ARCH}
       fi
       ;;
   mac)
