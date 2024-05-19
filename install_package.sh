07857056c42f0a4e5a66821a8672af42b217c581
John <m@DESKTOP-7TRGB04.localdomain>
2024-05-19 11:16:52 +0800

    fix

--- install_package.sh
+++ install_package.sh
@@ -3,24 +3,24 @@
 set -ex
 
 wget $CLANG_URL
-tar xf clang.tar.xz -C /usr/local/
+sudo tar xf clang.tar.xz -C /usr/local/
 
 case ${OS} in
   linux)
       sudo -E apt-get -qq install binutils-${LINUX_TARGET} || true
       rootfs=$ROOTFS_URL/rootfs-${deb_arch}.tar.xz
       wget $rootfs
-      tar xf rootfs*.tar.xz -C /opt/
+      sudo tar xf rootfs*.tar.xz -C /opt/
       ;;
   win)
       sudo -E apt-get -qq install ninja-build
       xwin=$XWIN_URL/xwin-${WIN_ARCH}.tar.xz
       wget $xwin
-      tar xf xwin*.tar.xz -C /opt/
+      sudo tar xf xwin*.tar.xz -C /opt/
       ;;
   mac)
       wget $OSXCROSS_URL
-      tar xf osxcross.tar.xz -C /usr/local/
+      sudo tar xf osxcross.tar.xz -C /usr/local/
       ;;
 esac
 
