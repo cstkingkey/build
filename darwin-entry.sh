b033364d0110b25189b597fb922295f2fc9ed151
cstkingkey <cstkingkey@users.noreply.github.com>
2024-05-19 09:25:52 +0800

    Update darwin-entry.sh

--- darwin-entry.sh
+++ darwin-entry.sh
@@ -30,4 +30,4 @@ declare -x CARGO_TARGET_${upper_suffix}_LINKER="${tools_prefix}"-clang
 declare -x CFLAGS_${envvar_suffix}="-stdlib=libc++ -fuse-ld=${OSXCROSS_ROOT}/bin/${tools_prefix}-ld"
 declare -x CXXFLAGS_${envvar_suffix}="-stdlib=libc++ -fuse-ld=${OSXCROSS_ROOT}/bin/${tools_prefix}-ld"
 
-exec "$@"
+#exec "$@"
